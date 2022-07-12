52da9cea83b650b6f17f29ec6dda5011a91b025f
James Wootton <decodoku@gmail.com>
2022-07-12 12:32:55 +0200

    move to 0.02

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='decodoku',
       install_requires=['qiskit'],
-      version='0.0.1',
+      version='0.0.2',
       packages=['decodoku']
 )
